e4a800002ccdf1abfc73196b13bbec29269500f5
richardcsts <richard@foodpass.me>
2025-05-05 18:20:56 -0300

    fix: adding a missing file

---
+++ src/components/BoardHeader/BoardHeader.styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+
+export const LeftSide = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const RightSide = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Perfil = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  background-color: #5243aa;
+  font-size: 14px;
+  border-radius: 50%;
+  width: 32px;
+  height: 32px;
+  margin-right: 8px;
+  color: #fff;
+`;
